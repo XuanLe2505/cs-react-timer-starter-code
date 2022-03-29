bf97f4d8fce1c9096378466709698438a366d92d
XuanLe <leanhxuan051996@gmail.com>
2022-03-30 02:19:55 +0700

    timer app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import "Your code here";
+import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
+import "./App.css";
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+  const {
+    time,
+    isReset,
+    startTimer,
+    stopTimer,
+    resetTimer,
+    active,
+    activeSplit,
+    splitTimer,
+    splitList,
+  } = useTimer(0);
 
   return (
     <div className="App container">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{"Your code here"}</p>
+          <p>{formatTime(time)}</p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={stopTimer}>
             Stop
+          </button>
+          <button className="button" ref={active} onClick={startTimer}>
+            Start
+          </button>
+          <button className="button" onClick={resetTimer}>
+            Reset
           </button>
           <button
             className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
+            disabled={isReset}
+            ref={activeSplit}
+            onClick={splitTimer}
           >
-            Start
-          </button>
-          <button className="button" onClick={"Your code here"}>
-            Reset
+            Split
           </button>
         </div>
+      </div>
+      {splitList.length > 0 && <hr />}
+      <div className="split-list">
+        {splitList.map(({ time, reason }, i) => {
+          return (
+            <div className="split-item" key={time}>
+              <div>#{i + 1}</div>
+              <div className="split">{formatTime(time)}</div>
+              <div>{reason}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,53 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
-
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const [time, setTime] = useState(ini);
+  const [splitList, setSplitList] = useState([]);
+  const [isStart, setIsStart] = useState(false);
+  const active = useRef();
+  const activeSplit = useRef();
+  const refInterval = useRef();
 
   const startTimer = () => {
-    "Your code here";
+    setIsStart(!isStart);
+    refInterval.current = setInterval(() => {
+      setTime((second) => second + 1);
+    }, 1000);
     active.current.disabled = true;
+    activeSplit.current.disabled = false;
   };
   const stopTimer = () => {
-    "Your code here";
+    clearInterval(refInterval.current);
+    active.current.disabled = false;
+    activeSplit.current.disabled = true;
   };
   const resetTimer = () => {
-    "Your code here";
+    setIsStart(isStart);
+    clearInterval(refInterval.current);
+    setTime(0);
+    setSplitList([]);
     active.current.disabled = false;
   };
 
-  return { time, startTimer, stopTimer, resetTimer, active };
+  const splitTimer = () => {
+    addSplitValue("Split");
+  };
+  const addSplitValue = (reason) => {
+    setSplitList((split) => [...split, { time, reason }]);
+  };
+
+  const isReset = time === 0;
+
+  return {
+    time,
+    isReset,
+    startTimer,
+    stopTimer,
+    resetTimer,
+    active,
+    activeSplit,
+    splitTimer,
+    splitList,
+  };
 };
 export default useTimer;
